f012c4d12fb09a6f10804338ddc887ad64b6379d
Shinribo <73356576+Shinribo@users.noreply.github.com>
2024-09-27 20:30:15 +0200

    Implemented Interrupt Handling in Hal and x86_64 modules

--- src/hal/interrupt.rs
+++ src/hal/interrupt.rs
@@ -1,1 +1,19 @@
-pub(crate) const EXAMPLE_INTERRUPT_LEVEL: u64 = 0; //arch::interrupt::get_example_interrupt_level();
+
+pub(crate) type IrqLevel = super::arch::interrupt::ArchIrqLevel;
+
+pub(crate) const MASK_ALL: IrqLevel = super::arch::interrupt::MASK_ALL;
+
+
+
+
+///Is reentrant safe as the IrqLevel is a core local hardware mechanism
+pub(super) unsafe fn set_irq_level(value: IrqLevel){
+    super::arch::interrupt::set_irq_level(value);
+}
+
+///Is reentrant safe as the IrqLevel is a core local hardware mechanism \
+///Increases the IrqLevel to the requested level if the current Level is lower \
+///returns the old IrqLevel
+pub(super) unsafe  fn bump_irq_level(value: IrqLevel) -> IrqLevel{
+    super::arch::interrupt::bump_irq_level(value)
+}
--- src/hal/x86_64/interrupt.rs
+++ src/hal/x86_64/interrupt.rs
@@ -1,1 +1,34 @@
 //wrapper around cpu specific details for needed function
+
+use core::arch::asm;
+
+use crate::hal::interrupt::IrqLevel;
+
+#[derive(Clone, Copy)]
+pub struct ArchIrqLevel(u8);
+
+
+pub(in crate::hal) const MASK_ALL: IrqLevel = ArchIrqLevel(15);
+
+
+pub(in crate::hal) unsafe fn set_irq_level(value: IrqLevel) {
+    asm!(
+        " mov cr8, {}",
+        in(reg_byte) value.0,
+    )
+}
+
+pub(in crate::hal) unsafe fn bump_irq_level(value: IrqLevel) -> IrqLevel {
+    
+    let mut value_new: u8;
+    
+    asm!(
+        " mov {0}, cr8",
+        " movzbq cr8, {1}",
+        out(reg_byte) value_new,
+        in(reg_byte) value.0,
+    );
+
+    ArchIrqLevel(value_new)
+
+}
